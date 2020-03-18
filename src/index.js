9f2101c6299307c93ab6d68ca2cd2b45c4b776a6
Samir Harry Huamán Patricio <shuamanp@uni.pe>
2020-03-18 00:17:37 -0500

    Segundo Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const TarjetaFruta = (props) => (
+	<div>
+		<h3>{props.name}</h3>
+		<hr />
+		<p> $ {props.price}</p>
+	</div>
+)
+
+const App = () => (
+	<div>
+		<TarjetaFruta name='Sandia' />
+		<TarjetaFruta name='Naranja' price={2.00} />
+		<TarjetaFruta name='Naranja' />
+		<TarjetaFruta name='Naranja' />
+		<TarjetaFruta name='Naranja' />
+		<TarjetaFruta name='Naranja' />
+		<TarjetaFruta name='Naranja' />
+		<TarjetaFruta name='Naranja' >
+		</TarjetaFruta>
+	</div>
+)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
